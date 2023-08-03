e8f920b7d1ae5c345f9421bdebe79927a2bf04c5
snapiz <jonathan.lapiquonne@gmail.com>
2023-08-03 22:41:05 +0200

    fix: update deps

--- pikav-web/src/leptos.rs
+++ pikav-web/src/leptos.rs
@@ -1,6 +1,5 @@
 use cfg_if::cfg_if;
 use futures::Future;
-use leptos::Scope;
 use pikav::Event;
 use serde_json::Value;
 
@@ -9,69 +8,66 @@ use leptos::{on_cleanup, provide_context, use_context};
 
 use crate::Client;
 
-pub fn pikav_context(cx: Scope, client: Client) {
-    pikav_context_with_clients(cx, vec![client]);
+pub fn pikav_context(client: Client) {
+    pikav_context_with_clients(vec![client]);
 }
 
 cfg_if! {
     if #[cfg(feature = "leptos-hydrate")] {
-        pub fn pikav_context_with_clients(cx: Scope, clients: Vec<Client>) {
-            provide_context(cx, clients.clone());
+        pub fn pikav_context_with_clients(clients: Vec<Client>) {
+            provide_context(clients.clone());
 
-            on_cleanup(cx, move || clients.iter().for_each(|c| c.close()));
+            on_cleanup(move || clients.iter().for_each(|c| c.close()));
         }
     } else {
-        pub fn pikav_context_with_clients(_cx: Scope, _clients: Vec<Client>) {}
+        pub fn pikav_context_with_clients(_clients: Vec<Client>) {}
     }
 }
 
-pub fn use_client(cx: Scope) -> Client {
-    use_client_with(cx, 0)
+pub fn use_client() -> Client {
+    use_client_with(0)
 }
 
 cfg_if! {
     if #[cfg(feature = "leptos-hydrate")] {
-        pub fn use_client_with(cx: Scope, id: usize) -> Client {
-            use_context::<Vec<Client>>(cx)
+        pub fn use_client_with(id: usize) -> Client {
+            use_context::<Vec<Client>>()
                 .expect("Pikav provider not configured correctly")
                 .get(id)
                 .unwrap_or_else(|| panic!("to get client {id}"))
                 .clone()
         }
     } else {
-        pub fn use_client_with(_cx: Scope, _id: usize) -> Client {
+        pub fn use_client_with(_id: usize) -> Client {
             Client::new("")
         }
     }
 }
 
 pub fn use_subscribe<Fut>(
-    cx: Scope,
     filter: impl Into<String> + 'static,
     listener: impl Fn(Event<Value, Value>) -> Fut + 'static,
 ) where
     Fut: Future<Output = ()> + 'static + Send,
 {
-    use_subscribe_with(cx, 0, filter, listener);
+    use_subscribe_with(0, filter, listener);
 }
 
 cfg_if! {
     if #[cfg(feature = "leptos-hydrate")] {
         pub fn use_subscribe_with<Fut>(
-            cx: Scope,
             id: usize,
             filter: impl Into<String> + 'static,
             listener: impl Fn(Event<Value, Value>) -> Fut + 'static,
         ) where
             Fut: Future<Output = ()> + 'static + Send,
         {
-            let unsubscribe = use_client_with(cx, id).subscribe(filter, listener);
+            let unsubscribe = use_client_with(id).subscribe(filter, listener);
 
-            on_cleanup(cx, unsubscribe);
+            on_cleanup(unsubscribe);
         }
     } else {
         pub fn use_subscribe_with<Fut>(
-            _cx: Scope,
             _id: usize,
             _filter: impl Into<String> + 'static,
             _listener: impl Fn(Event<Value, Value>) -> Fut + 'static,
